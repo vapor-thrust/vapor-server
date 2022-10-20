9086cd24558b9cd3f7e00d830e985c82498bfa78
Hiroi1125 <76752625+Hiroi1125@users.noreply.github.com>
2022-10-20 17:07:36 +0900

    code edit

--- app/operate/operation.py
+++ app/operate/operation.py
@@ -1,18 +1,18 @@
 class Operation:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
 
     def plus(self):
-        return self.a + self.b
+        return self.first + self.second
 
     def minus(self):
-        return self.a - self.b
+        return self.first - self.second
 
     def mul(self):
-        return self.a * self.b
+        return self.first * self.second
 
     def div(self):
-        if self.b == 0:
+        if self.second == 0:
             return None
-        return self.a / self.b
+        return self.first / self.second
