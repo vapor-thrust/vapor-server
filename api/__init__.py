77512da38fd9fa12606a1a648ef10051dcdd131e
Hiroi1125 <76752625+Hiroi1125@users.noreply.github.com>
2022-10-20 15:27:54 +0900

    Practice service

--- api/__init__.py
+++ api/__init__.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter
 
 from api.user.v1.user import user_router as user_v1_router
 from api.auth.auth import auth_router
+from api.operate.operate import oper_router
 
 router = APIRouter()
 router.include_router(user_v1_router, prefix="/api/v1/users", tags=["User"])
 router.include_router(auth_router, prefix="/auth", tags=["Auth"])
-
+router.include_router(oper_router, prefix="/oper", tags=["Oper"])
 
 __all__ = ["router"]
